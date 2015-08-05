19f41a1bf8d7b4ad9ae3f835d255df94729f541c
Toparz01 <toparz20002000@gmail.com>
2015-08-05 21:37:24 +0900

    空コミット

---
+++ TypeScriptHTMLApp1/MainController.ts
@@ -0,0 +1,42 @@
+﻿module Main {
+
+    export class Value {
+        Text: string;
+    }
+
+    export interface Scope {
+        value: Value;
+        newText: string;
+        setValue: Function;
+    }
+
+    export class Controller {
+        private httpService: any;
+        constructor($scope: Scope, $http: any) {
+            this.httpService = $http;
+            var controller = this;
+            $scope.setValue = function () {
+                var value = new Main.Value();
+                value.Text = $scope.newText;
+                controller.postValue(value, function (data) {
+                    controller.getValue(function (data) {
+                        $scope.newText = "";
+                        $scope.value = data;
+                    });
+                });
+            }
+        }
+
+        getValue(successCallback: Function) {
+            this.httpService.get("/api/values").success(function (data, status) {
+                successCallback(data);
+            });
+        }
+
+        postValue(value: Value, successCallback: Function) {
+            this.httpService.post("/api/values", value).success(function (data, status) {
+                successCallback(data);
+            });
+        }
+    }
+}
---
+++ TypeScriptHTMLApp1/app.js
@@ -0,0 +1,38 @@
+var Main;
+(function (Main) {
+    var Value = (function () {
+        function Value() {
+        }
+        return Value;
+    })();
+    Main.Value = Value;
+    var Controller = (function () {
+        function Controller($scope, $http) {
+            this.httpService = $http;
+            var controller = this;
+            $scope.setValue = function () {
+                var value = new Main.Value();
+                value.Text = $scope.newText;
+                controller.postValue(value, function (data) {
+                    controller.getValue(function (data) {
+                        $scope.newText = "";
+                        $scope.value = data;
+                    });
+                });
+            };
+        }
+        Controller.prototype.getValue = function (successCallback) {
+            this.httpService.get("/api/values").success(function (data, status) {
+                successCallback(data);
+            });
+        };
+        Controller.prototype.postValue = function (value, successCallback) {
+            this.httpService.post("/api/values", value).success(function (data, status) {
+                successCallback(data);
+            });
+        };
+        return Controller;
+    })();
+    Main.Controller = Controller;
+})(Main || (Main = {}));
+//# sourceMappingURL=app.js.map
